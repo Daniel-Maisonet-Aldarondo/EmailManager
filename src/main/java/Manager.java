78b2c188e3fcd218dc7d64c37e90f60007b32378
Daniel E. Maisonet Aldarondo <danielmaisonetaldarondo@gmail.com>
2021-04-06 13:09:10 -0400

    initial commit

---
+++ src/main/java/Manager.java
@@ -0,0 +1,23 @@
+public class Manager {
+
+	//TODO
+	public static void send() {
+	}
+
+
+
+	//TODO
+	public static void get() {}
+
+	//TODO
+	public static void delete() {}
+
+
+
+	//TODO
+	public static void query() {}
+
+
+
+
+}
