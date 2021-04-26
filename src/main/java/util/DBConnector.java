60582f0030930c22e48f23350abd8a1bc778450e
Daniel E. Maisonet Aldarondo <danielmaisonetaldarondo@gmail.com>
2021-04-26 15:27:13 -0400

    added ttl index

--- src/main/java/util/DBConnector.java
+++ src/main/java/util/DBConnector.java
@@ -4,11 +4,14 @@ import com.mongodb.*;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.IndexOptions;
+import com.mongodb.client.model.Indexes;
 import org.bson.Document;
 
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class DBConnector {
     private static MongoClient mongo;
@@ -18,28 +21,18 @@ public class DBConnector {
 
 
     public DBConnector() {
-//        credential = MongoCredential.createCredential("root", "emailDB", "password".toCharArray());
-//        MongoClientURI uri = new MongoClientURI("mongodb://root:password@localhost/?authSource=db1");
-//        mongo = new MongoClient(uri);
-
-//        mongo = new MongoClient(new ServerAddress("localhost", 27017), Arrays.asList(credential));
         mongo=  new MongoClient();
         database = mongo.getDatabase("emailDB");
-
     }
 
     public void updateBackups(List<Email> emails) {
         MongoCollection<Document> collection = database.getCollection("backups");
+        collection.createIndex(Indexes.ascending("date"), new IndexOptions().expireAfter(1L, TimeUnit.DAYS));
         for(Email email: emails) {
             collection.insertOne(new Document("email", email.toString()));
         }
     }
 
-//    private static Document DBObject createDBObject(Email email) {
-//        Document document = new Document();
-//        document.append("email", email.toString());
-//        return document;
-//    }
 
     public void displayBackups() {
         MongoCollection<Document> collection = database.getCollection("backups");
